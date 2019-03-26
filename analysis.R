b9b3c2d03f5237e6f6e1e5088c0c0f890b6dfd00
kimoyerr <kimoyerr@gmail.com>
2019-03-26 08:11:15 +0000

    new column and collect

--- analysis.R
+++ analysis.R
@@ -3,6 +3,7 @@ library(mongolite)
 con <- mongo("crispr_tweets", url = "mongodb+srv://kimoyerr:mohan921983@tweetdb-1f6vz.mongodb.net/tweets")
 con$count()
 
+library(dplyr) # Always load dplyr before sparkR
 Sys.setenv(SPARK_HOME = '/usr/lib/spark')
 library(SparkR, lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))
 my_spark <- sparkR.session(
@@ -15,3 +16,17 @@ my_spark <- sparkR.session(
 
 df <- read.df("", source = "com.mongodb.spark.sql.DefaultSource")
 printSchema(df)
+head(select(df, "tweet_created"))
+head(count(groupBy(df, df$user_name)),100)
+# Create a new day field
+df = df %>% withColumn("day", to_date(.$tweet_created,  "ddMMMyy"))
+gen = count(groupBy(df, df$day))
+printSchema(gen)
+tmp =collect(gen)
+
+
+
+library(sparklyr,lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))
+
+sdf_register(df)
+
